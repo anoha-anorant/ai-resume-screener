b59c2f5bf363625b36ee52c8ed6335c73d038715
Bittu Kumar <bittukumar06082001@gmail.com>
2026-05-30 14:10:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 # =========================================================
 
 st.set_page_config(
-    page_title="SmartHire AI",
+    page_title="UseAI for Hiring",
     page_icon="🤖",
     layout="wide",
     initial_sidebar_state="expanded"
